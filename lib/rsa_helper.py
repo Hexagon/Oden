31d60f513f1d00ebd652e6afb00da4c7529eae15
Hexagon <robinnilsson@gmail.com>
2010-12-15 18:15:19 +0100

    Generating keys with pycrypto istead of openssl binaries

--- lib/rsa_helper.py
+++ lib/rsa_helper.py
@@ -1,51 +1,9 @@
 import config
-from subprocess import call
+from Crypto.PublicKey import RSA
 
-# Generate RSA by _using system calls_
-# ( using python rsa lib was too slow for now )
+# Generate a RSA key pair using Crypto.PublicKey.RSA
 def generate_rsa(bits):
-
-    public_path = config.temp_path+"publ.key"
-    private_path = config.temp_path+"priv.key"
-
-    retcode_private = call([config.openssl_executable, "genrsa", "-out", private_path, "%i" % (bits)],shell=False)
-
-    if retcode_private == 0:
-        retcode_public = call([config.openssl_executable, "rsa", "-in", private_path, "-pubout", "-out",public_path],shell=False)
-        if retcode_public == 0:
-
-            # Try reading
-            try:
-                f = open(public_path, 'r')
-                public_key = f.read()
-                f.close()
-
-                f = open(private_path, 'r')
-                private_key = f.read()
-                f.close()
-
-                return [public_key,private_key]
-            except:
-                pass
-
-            # Clean up
-            try:
-                call(["rm","-f",private_path],shell=False)
-                call(["rm","-f",public_path],shell=False)
-            except:
-                pass
-
-            return None
-        
-        else:
-
-            # Clean private
-            try:
-                call(["rm","-f",private_path],shell=False)
-            except:
-                pass
-
-            return None
-    else:
-
-        return None
+    key = RSA.generate(4096)
+    private = key.exportKey('PEM')
+    public = key.publickey().exportKey('PEM')
+    return [public,private]
